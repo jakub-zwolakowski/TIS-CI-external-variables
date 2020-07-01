b2863d22943b82fd63ca1fe2d57865be0d1a3b0d
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-07-01 16:52:22 +0200

    Second test with the option -context-uninitialized-extern

---
+++ run.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+tis-kernel -val -val-profile "interpreter" -no-deps-show-progress -no-val-print -info-json-results test1.json -tis-config-load tis.config -tis-config-select 1
+tis-kernel -val -val-profile "interpreter" -no-deps-show-progress -no-val-print -info-json-results test2.json -tis-config-load tis.config -tis-config-select 2
